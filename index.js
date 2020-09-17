4044473663f09aad54a39a1320574d182749441a
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-09-17 21:48:22 +0200

    Add formatting to money and split

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+const formatMoney = (value) => { 
+  value = Math.ceil(value * 100) / 100;
+  return value.toFixed(2) + ' e';
+};
+
+const formatSplit = (value) => { 
+  if (value === '1') {
+    return value + ' person';
+  };
+
+  return value + ' people';
+};
+
 const update = () => {
   const bill = Number(document.querySelector('#yourBill').value);
   const tipPercent = document.querySelector('#tipInput').value;
   const split = document.querySelector('#splitInput').value;
-  
+
   let tipValue = bill * (tipPercent / 100);
   let tipEach = tipValue / split;
   let totalEach = (bill + tipValue) / split;
 
   let tipAmount = document.querySelector('#tipPercent').innerHTML = tipPercent + '%';
 
-  let tip = document.querySelector('#tipValue').innerHTML = tipValue;
-  let total = document.querySelector('#totalWithTip').innerHTML = bill + tipValue;
-  let splitBetween = document.querySelector('#splitValue').innerHTML = split;
-  let billIndividual = document.querySelector('#billEach').innerHTML = totalEach;
-  let tipIndividual = document.querySelector('#tipEach').innerHTML = tipEach;
+  let tip = document.querySelector('#tipValue').innerHTML = formatMoney(tipValue);
+  let total = document.querySelector('#totalWithTip').innerHTML = formatMoney(bill + tipValue);
+  let splitBetween = document.querySelector('#splitValue').innerHTML = formatSplit(split);
+  let billIndividual = document.querySelector('#billEach').innerHTML = formatMoney(totalEach);
+  let tipIndividual = document.querySelector('#tipEach').innerHTML = formatMoney(tipEach);
 
 }
 
