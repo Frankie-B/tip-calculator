1eafcccb1c013b6119cfac784023d7526152e818
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-09-17 21:13:21 +0200

    Add Dynamic values

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const update = () => {
-  const bill = document.querySelector('#yourBill').value;
+  const bill = Number(document.querySelector('#yourBill').value);
   const tipPercent = document.querySelector('#tipInput').value;
   const split = document.querySelector('#splitInput').value;
-  console.log('I was clicked', { bill, tipPercent, split });
+  
+  let tipValue = bill * (tipPercent / 100);
+  let tipEach = tipValue / split;
+  let totalEach = (bill + tipValue) / split;
+
+  let tipAmount = document.querySelector('#tipPercent').innerHTML = tipPercent + '%';
+
+  let tip = document.querySelector('#tipValue').innerHTML = tipValue;
+  let total = document.querySelector('#totalWithTip').innerHTML = bill + tipValue;
+  let splitBetween = document.querySelector('#splitValue').innerHTML = split;
+  let billIndividual = document.querySelector('#billEach').innerHTML = totalEach;
+  let tipIndividual = document.querySelector('#tipEach').innerHTML = tipEach;
+
 }
 
 let container = document.querySelector('#container');
